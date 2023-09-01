b8ff11e0217d25804a5b3cc5422571278a9d28d5
sfjbs <bhavesh.jaiswani@tcs.com>
2023-09-01 13:35:03 +0530

    first maven project

---
+++ Day01/simple-maven-app/src/main/java/com/bhavesh/data/App.java
@@ -0,0 +1,13 @@
+package com.bhavesh.data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! bhavesh" );
+    }
+}
