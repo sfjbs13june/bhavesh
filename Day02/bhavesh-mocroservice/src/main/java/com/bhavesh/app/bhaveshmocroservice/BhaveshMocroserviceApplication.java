057c1024d1f320191d7c0a00616274149724e36e
sfjbs <bhavesh.jaiswani@tcs.com>
2023-09-04 11:54:32 +0530

    first microservice project

---
+++ Day02/bhavesh-mocroservice/src/main/java/com/bhavesh/app/bhaveshmocroservice/BhaveshMocroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.bhavesh.app.bhaveshmocroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhaveshMocroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BhaveshMocroserviceApplication.class, args);
+	}
+
+}
---
+++ Day02/bhavesh-mocroservice/src/test/java/com/bhavesh/app/bhaveshmocroservice/BhaveshMocroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhavesh.app.bhaveshmocroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhaveshMocroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
